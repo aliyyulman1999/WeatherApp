a49fb1bc7c7a2dec72eea98d65603bf2be697eaa
Aliyyulman Jihan <49016762+aliyyulman1999@users.noreply.github.com>
2020-06-24 09:52:46 +0700

    "first commit"

---
+++ app/src/main/java/com/rplgdc/weatherapp/Model.java
@@ -0,0 +1,19 @@
+package com.rplgdc.weatherapp;
+
+public class Model {
+
+    private int image;
+
+    public Model(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+}
